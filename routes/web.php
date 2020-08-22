9191ea19c2f539d09e8115b542b06c4d1793c79e
Alew <alejandrow@tmachine.com.uy>
2020-08-22 19:22:07 -0300

    Master webs Full Stack: Angular, Node, Laravel, Symfony. Initial config

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    //echo "<h1>Hola Mundo</h1>";
+});
+
+
+/*
+GET: Conseguir datos
+POST: Guardar datos
+PUT: Actualizar datos
+DELETE: Eliminar datos
+*/
+
+Route::get('/hola', function(){
+    //return view('welcome');
+    echo "<h1>Hola Mundo</h1>";
+});
+
+Route::get('/pruebas/{nombre?}', function($nombre=null){
+    $texto = '<h2>Texto de una ruta</>';
+    $texto .= 'Nombre: '.$nombre;
+    return  view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+
+Route::get('animales', 'PruebasController@index');
